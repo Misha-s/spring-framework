8d3b94a0ece95a5897a6a8feb97ffe2ff183ff5a
Mike <miksid@protonmail.com>
2023-08-31 17:38:09 -0400

    @Qualifier

--- spring-core-Abstraction-04/src/main/java/com/tomato/proxy/EmailCommentNotificationProxy.java
+++ spring-core-Abstraction-04/src/main/java/com/tomato/proxy/EmailCommentNotificationProxy.java
@@ -1,9 +1,13 @@
 package com.tomato.proxy;
 
 import com.tomato.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary
+@Qualifier("email")
 public class EmailCommentNotificationProxy implements CommentNotificationProxy {
     @Override
     public void sendComment(Comment comment) {
--- spring-core-Abstraction-04/src/main/java/com/tomato/service/CommentService.java
+++ spring-core-Abstraction-04/src/main/java/com/tomato/service/CommentService.java
@@ -3,6 +3,7 @@ package com.tomato.service;
 import com.tomato.model.Comment;
 import com.tomato.proxy.CommentNotificationProxy;
 import com.tomato.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,12 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+//    public CommentService(CommentRepository commentRepository,@Qualifier("commentPushNotification") CommentNotificationProxy commentNotificationProxy) {
+//        this.commentRepository = commentRepository;
+//        this.commentNotificationProxy = commentNotificationProxy;
+//    }
+
+    public CommentService(CommentRepository commentRepository,@Qualifier("push") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
