f67abd5109b68bcf7ad18f23f29d5cf72ada3eda
Mike <miksid@protonmail.com>
2023-09-05 17:27:41 -0400

    SpringBoot

---
+++ spring-SpringBoot-06/src/test/java/com/cherry/springspringboot06/SpringSpringBoot06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.springspringboot06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpringBoot06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
