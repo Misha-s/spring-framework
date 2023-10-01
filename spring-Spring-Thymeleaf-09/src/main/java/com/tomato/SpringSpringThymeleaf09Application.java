ff4fcdba7f797c49a2506198cab9693e393747c1
Mike <miksid@protonmail.com>
2023-10-01 12:55:05 -0400

    Spring Thymeleaf

---
+++ spring-Spring-Thymeleaf-09/src/main/java/com/tomato/SpringSpringThymeleaf09Application.java
@@ -0,0 +1,13 @@
+package com.tomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpringThymeleaf09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpringThymeleaf09Application.class, args);
+    }
+
+}
